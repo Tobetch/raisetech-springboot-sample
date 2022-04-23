41db46420fa827bedf4906fb4249197d8d31718b
Tobetch <takutobe@gmail.com>
2022-04-23 16:02:05 +0900

    add UserRestApiController

--- src/main/java/com/example/demo/controller/UserRestApiController.java
+++ src/main/java/com/example/demo/controller/UserRestApiController.java
@@ -3,29 +3,23 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
 
-@Controller
-public class TopController {
+@RestController
+public class UserRestApiController {
 
 	@Autowired
 	UserService userService;
 
-	@GetMapping
-	public String index(Model m) {
-
-		m.addAttribute("message", "wasshoi");
-
+	@GetMapping("/api")
+	public List<User> getUsers() {
 		// ユーザー情報取得
 		List<User> userList = userService.getUsers();
-		System.out.println(userList);
-		m.addAttribute(userList);
-
-		return "index";
+		return userList;
 	}
+
 }
