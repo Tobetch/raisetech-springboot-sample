e4655614376a9ee103d119fed02c385d57ed9e06
Tobetch <takutobe@gmail.com>
2022-04-23 14:56:57 +0900

    first try for Thymeleaf

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +12,9 @@ public class TopController {
 //	todo: add service by DI
 
 	@GetMapping
-	public String index() {
+	public String index(Model m) {
+		String messageString = "wasshoi";
+		m.addAttribute("message", messageString);
 		return "index";
 	}
 }
